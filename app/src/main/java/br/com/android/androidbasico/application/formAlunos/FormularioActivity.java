3cf3ee188d43dad7c832fd0e1642c63aeb2cfc66
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 01:59:42 -0200

    Adicionar evento de click no botao salvar do formulario

--- app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
+++ app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
@@ -2,14 +2,25 @@ package br.com.android.androidbasico.application.formAlunos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import br.com.android.androidbasico.R;
 
 public class FormularioActivity extends AppCompatActivity {
-
+    private Button mBotaoSalvar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        mBotaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        mBotaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormularioActivity.this,"Evento de click",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
