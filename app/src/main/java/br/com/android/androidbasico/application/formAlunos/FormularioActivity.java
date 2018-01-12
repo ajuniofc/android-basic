c9cd163a2632fcbc30dfd2f8665bbf6ad0dbcd8c
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 01:13:03 -0200

    Criar activity de formulario de cadastro de aluno

---
+++ app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
@@ -0,0 +1,15 @@
+package br.com.android.androidbasico.application.formAlunos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.android.androidbasico.R;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
