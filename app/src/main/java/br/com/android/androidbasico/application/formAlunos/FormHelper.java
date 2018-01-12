9bf4b7c1a7fe1e330bedb5c6c1505a112bb49b65
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 21:44:14 -0200

    Criar FormHelper

---
+++ app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormHelper.java
@@ -0,0 +1,18 @@
+package br.com.android.androidbasico.application.formAlunos;
+
+import br.com.android.androidbasico.R;
+
+/**
+ * Created by JHUNIIN on 12/01/2018.
+ */
+
+public class FormHelper {
+
+    public FormHelper(FormularioActivity activity) {
+        activity.findViewById(R.id.formulario_nome);
+        activity.findViewById(R.id.formulario_endereco);
+        activity.findViewById(R.id.formulario_telefone);
+        activity.findViewById(R.id.formulario_site);
+        activity.findViewById(R.id.formulario_nome);
+    }
+}
