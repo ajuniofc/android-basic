f07e5404055869e002e6d170b5e372751d42a241
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 21:25:11 -0200

    Remover botao de salvar do formulario

--- app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
+++ app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
@@ -11,19 +11,12 @@ import android.widget.Toast;
 import br.com.android.androidbasico.R;
 
 public class FormularioActivity extends AppCompatActivity {
-    private Button mBotaoSalvar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        mBotaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-        mBotaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(FormularioActivity.this,"Evento de click",Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
