420b70b0fb62e591f9cfdced960e6b276f3b14e9
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 19:25:01 -0200

    Adicionar evento de click no botao de adicionar aluno

--- app/src/main/java/br/com/android/androidbasico/application/listaAlunos/ListaAlunosActivity.java
+++ app/src/main/java/br/com/android/androidbasico/application/listaAlunos/ListaAlunosActivity.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import br.com.android.androidbasico.R;
 
 public class ListaAlunosActivity extends AppCompatActivity {
-
+    private Button mButaoAdicionar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        mButaoAdicionar = (Button) findViewById(R.id.lista_btn_adicionarId);
+        mButaoAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
