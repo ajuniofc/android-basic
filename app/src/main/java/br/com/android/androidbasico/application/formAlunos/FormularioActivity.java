9d4356beab9dddb3aafb77e5efe7e9ecc95f0c22
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 21:19:45 -0200

    Adicionar menu na activity do formulario

--- app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
+++ app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormularioActivity.java
@@ -2,6 +2,8 @@ package br.com.android.androidbasico.application.formAlunos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,5 +24,22 @@ public class FormularioActivity extends AppCompatActivity {
                 Toast.makeText(FormularioActivity.this,"Evento de click",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.formulario_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this,"Aluno Salvo",Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
