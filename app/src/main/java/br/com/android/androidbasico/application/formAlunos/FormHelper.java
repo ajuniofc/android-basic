fdb2a03298fab96bb3429a875fdddba562c4143e
Antonio Junio <a.junio.fc@gmail.com>
2018-01-12 22:00:31 -0200

    Implementar FormHelper

--- app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormHelper.java
+++ app/src/main/java/br/com/android/androidbasico/application/formAlunos/FormHelper.java
@@ -1,18 +1,36 @@
 package br.com.android.androidbasico.application.formAlunos;
 
+import android.widget.EditText;
+
 import br.com.android.androidbasico.R;
+import br.com.android.androidbasico.model.Aluno;
 
 /**
  * Created by JHUNIIN on 12/01/2018.
  */
 
 public class FormHelper {
+    private final EditText edtNome;
+    private final EditText edtEndereco;
+    private final EditText edtTelefone;
+    private final EditText edtSite;
+    private final EditText edtNota;
 
     public FormHelper(FormularioActivity activity) {
-        activity.findViewById(R.id.formulario_nome);
-        activity.findViewById(R.id.formulario_endereco);
-        activity.findViewById(R.id.formulario_telefone);
-        activity.findViewById(R.id.formulario_site);
-        activity.findViewById(R.id.formulario_nome);
+        edtNome = (EditText) activity.findViewById(R.id.formulario_nome);
+        edtEndereco = (EditText) activity.findViewById(R.id.formulario_endereco);
+        edtTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
+        edtSite = (EditText) activity.findViewById(R.id.formulario_site);
+        edtNota = (EditText) activity.findViewById(R.id.formulario_nome);
+    }
+
+    public Aluno getAluno(){
+        Aluno aluno = new Aluno();
+        aluno.setNome(edtNome.getText().toString());
+        aluno.setEndereco(edtEndereco.getText().toString());
+        aluno.setTelefone(edtTelefone.getText().toString());
+        aluno.setSite(edtSite.getText().toString());
+        aluno.setNota(Double.valueOf(edtNota.getText().toString()));
+        return aluno;
     }
 }
