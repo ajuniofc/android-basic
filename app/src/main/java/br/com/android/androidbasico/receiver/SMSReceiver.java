b831ed7f7fc20a7df0f6ec9fe850b005f3e4ab60
Antonio Junio <a.junio.fc@gmail.com>
2018-01-14 11:40:11 -0200

    Criar SMSReceiver

---
+++ app/src/main/java/br/com/android/androidbasico/receiver/SMSReceiver.java
@@ -0,0 +1,16 @@
+package br.com.android.androidbasico.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by JHUNIIN on 14/01/2018.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        g
+    }
+}
