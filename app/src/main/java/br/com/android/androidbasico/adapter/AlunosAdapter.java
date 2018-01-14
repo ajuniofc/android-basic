731b78897a0ca56aa7142f88eb369586b7f8f11c
Antonio Junio <a.junio.fc@gmail.com>
2018-01-14 11:21:36 -0200

    Mostrar lista de aluno com o layout list_itens

--- app/src/main/java/br/com/android/androidbasico/adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/android/androidbasico/adapter/AlunosAdapter.java
@@ -1,13 +1,20 @@
 package br.com.android.androidbasico.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import br.com.android.androidbasico.R;
+import br.com.android.androidbasico.application.listaAlunos.AlunoHolder;
 import br.com.android.androidbasico.model.Aluno;
 
 /**
@@ -39,10 +46,39 @@ public class AlunosAdapter extends BaseAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
-        TextView view = new TextView(context);
+    public View getView(int position, View convertView, ViewGroup parent) {
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+        AlunoHolder holder;
+        View view = convertView;
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_itens, parent, false);
+            holder = new AlunoHolder(view);
+            view.setTag(holder);
+        }else {
+            view = convertView;
+            holder = (AlunoHolder) view.getTag();
+        }
+
+        showAluno(aluno, holder);
+
         return view;
+    }
+
+    private void showAluno(Aluno aluno, AlunoHolder holder) {
+        holder.nome.setText(aluno.getNome());
+        holder.telefone.setText(aluno.getTelefone());
+        carregaImg(aluno, holder);
+    }
+
+    private void carregaImg(Aluno aluno, AlunoHolder holder) {
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if (caminhoFoto != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            holder.foto.setImageBitmap(bitmapReduzido);
+            holder.foto.setScaleType(ImageView.ScaleType.FIT_XY);
+            holder.foto.setTag(caminhoFoto);
+        }
     }
 }
