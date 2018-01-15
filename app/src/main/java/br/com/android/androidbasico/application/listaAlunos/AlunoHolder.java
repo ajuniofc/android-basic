2e8404babe26f9269495bf1dcb359a0cee1f777a
Antonio Junio <a.junio.fc@gmail.com>
2018-01-14 22:16:08 -0200

    Adicionar campos de endereco e site no Holder

--- app/src/main/java/br/com/android/androidbasico/application/listaAlunos/AlunoHolder.java
+++ app/src/main/java/br/com/android/androidbasico/application/listaAlunos/AlunoHolder.java
@@ -14,10 +14,14 @@ public class AlunoHolder {
     public final ImageView foto;
     public final TextView nome;
     public final TextView telefone;
+    public final TextView endereco;
+    public final TextView site;
 
     public AlunoHolder(View view) {
         foto     = (ImageView) view.findViewById(R.id.item_foto);
         nome     = (TextView) view.findViewById(R.id.item_nome);
         telefone = (TextView) view.findViewById(R.id.item_telefone);
+        endereco = (TextView) view.findViewById(R.id.item_endereco);
+        site = (TextView) view.findViewById(R.id.item_site);
     }
 }
