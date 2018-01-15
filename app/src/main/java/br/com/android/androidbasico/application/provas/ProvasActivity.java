152e8b5062c1575d9a3a6024acb8db0d995cd85f
Antonio Junio <a.junio.fc@gmail.com>
2018-01-14 22:28:56 -0200

    Criar activity de provas

---
+++ app/src/main/java/br/com/android/androidbasico/application/provas/ProvasActivity.java
@@ -0,0 +1,15 @@
+package br.com.android.androidbasico.application.provas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.android.androidbasico.R;
+
+public class ProvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provas);
+    }
+}
